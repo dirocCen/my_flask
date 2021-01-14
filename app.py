bd0c387703a3407d54035421a4006de6be3a691e
cenyj <15521284374@163.com>
2021-01-14 19:48:50 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -73,26 +73,21 @@ def forge():          # 添加数据进data.db
     click.echo('Done.')
 
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+@app.context_processor       # 多个模板内都要使用的变量，可以在这里先定义好
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)     # 等同于 return {'user':user},这个函数返回的变量可以直接在模板中使用
+
+
+@app.errorhandler(404)    # 传入要处理的错误代码
+def page_not_found(e):      # 接受异常对象作为参数
+    return render_template('404.html'), 404
 
 
 @app.route('/')
 def index():
-    user = User.query.first()    # 读取用户记录
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)     # render_template传入的文件必须在根目录的template文件夹下
+    return render_template('index.html', movies=movies)     # render_template传入的文件必须在根目录的template文件夹下
 
 
 @app.route('/1')
